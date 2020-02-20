f115f2ddbd3b462cac30a697653cccc0980dbfdc
Paul Pearson <eda.paul@yahoo.co.uk>
2020-02-20 17:51:17 +0000

    bjr to hello

--- ppgit1/Form1.vb
+++ ppgit1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        TextBox1.Text = "Bjr"
+        TextBox1.Text = "Hello"
         TextBox2.Text = "World"
         TextBox3.Text = "of gits"
     End Sub
